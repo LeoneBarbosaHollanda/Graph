10472768071c6099cf7e179c5c107799808cd65f
LeoneBarbosaHollanda <leone.hollanda@hotmail.com>
2023-12-07 20:13:29 -0300

    Mario morrendo e codigo reniciando

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-import subprocess
-import os
+import cv2
+import numpy as np
+import pyautogui
+import time
 
-# Definir o caminho para o emulador e o jogo
-emulador_executavel = "Mesen.exe"
-rom_caminho = "mario.nes" # Substitua pelo caminho correto do jogo
 
-# Verificar se o arquivo ROM existe
-if not os.path.exists(rom_caminho):
-    raise FileNotFoundError(f"O arquivo ROM não foi encontrado: {rom_caminho}")
 
-# Iniciar o emulador com o jogo
-subprocess.run([emulador_executavel, rom_caminho])
+# Carregando a imagem do template (a tela de "Game Over", por exemplo)
+template = cv2.imread('MARIOF.jpg', 0)
+w, h = template.shape[::-1]
+
+# Verifica se o template foi carregado corretamente
+
+
+
+
+# Aplicando a correspondência de template
+
+
+
+
+
+
+time.sleep(10)  
+pyautogui.keyDown('k')
+pyautogui.keyUp('k')
+pyautogui.keyDown('d')
+while(True):
+    screenshot = pyautogui.screenshot()
+    gray = cv2.cvtColor(np.array(screenshot), cv2.COLOR_BGR2GRAY)
+    res = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
+    threshold = 0.8
+    loc = np.where(res >= threshold)
+
+    if len(loc[0]) > 0:
+        print("Imagem encontrada na tela!")
+        pyautogui.keyDown('f1')
+        pyautogui.keyUp('f1')
+    pass
